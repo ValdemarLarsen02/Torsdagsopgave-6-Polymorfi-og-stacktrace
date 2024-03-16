9332fe65340fe46495f7a3172bdd93c7c5685537
Valdemar <cph-vl91@cphbusiness.dk>
2024-03-16 01:44:38 +0100

    Task1

---
+++ Task1/Flows.java
@@ -0,0 +1,32 @@
+package Task1;
+
+public class Flows {
+    
+        
+        public void methodA() {
+            System.out.print("J");
+            System.out.print("a");
+        }
+        
+        
+        public void methodB() {
+            System.out.print("v");
+            System.out.print("a");
+            System.out.print(" ");
+            System.out.print("e");
+            System.out.print("r");
+        }
+        
+        
+        public void methodC() {
+            System.out.print(" ");
+            System.out.print("s");
+            System.out.print("j");
+        }
+        
+        public void methodD() {
+            System.out.print("o");
+            System.out.print("v");
+            System.out.print("t");
+        }
+}
---
+++ Task1/Main.java
@@ -0,0 +1,15 @@
+package Task1;
+
+public class Main {
+    public static void main(String[] args){
+        Flows f = new Flows();
+
+        f.methodA();
+        f.methodB();
+        f.methodC();
+        f.methodD();
+
+    }
+
+
+}
